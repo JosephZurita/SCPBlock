564433fa0e338e5225f3c6742672cc4145d1aa9e
Joseph Zurita <JosephZurita@hotmail.com>
2023-07-24 01:17:11 +0100

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,39 @@
+﻿using Exiled.API.Features.Roles;
+using Exiled.API.Interfaces;
+using PlayerRoles;
+using PluginAPI.Roles;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace SCPBlock
+{
+
+    /// <inheritdoc cref="IConfig"/>
+    public sealed class Config : IConfig
+    {
+
+        private static List<RoleTypeId> defBlocked = new() { RoleTypeId.Scp079 };
+
+        /// <inheritdoc/>
+        public bool IsEnabled { get; set; } = true;
+
+        /// <inheritdoc />
+        public bool Debug { get; set; }
+
+        /// <summary>
+        /// Gets the list of ints config.
+        /// </summary>
+        [Description("List of blocked SCPs")]
+        public List<RoleTypeId> blockList { get; private set; } = new(defBlocked);
+
+        /// <summary>
+        /// Gets the list of ints config.
+        /// </summary>
+        [Description("List of blocked SCPs")]
+        public List<RoleTypeId> swapList { get; private set; } = new(Enum.GetValues(typeof(RoleTypeId)).Cast<RoleTypeId>().Where(rt => rt.GetTeam() == Team.SCPs && !defBlocked.Contains(rt)));
+
+    }
+}
---
+++ SCPBLock.cs
@@ -0,0 +1,48 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using PlayerRoles;
+
+using System;
+
+using Player = Exiled.Events.Handlers.Player;
+
+namespace SCPBlock
+{
+    public class SCPBlock : Plugin<Config>
+    {
+        /// <inheritdoc />
+        public override string Author => "JZ";
+
+        /// <inheritdoc />
+        public override string Name => "SCPBlock";
+
+        /// <inheritdoc />
+        public override string Prefix => Name;
+
+        /// <inheritdoc />
+        public override Version RequiredExiledVersion => new Version(7, 2, 0);
+
+        /// <inheritdoc />
+        public override Version Version => new Version(1, 0, 0);
+
+        /// <inheritdoc/>
+        public override void OnEnabled()
+        {
+            Player.ChangingRole += OnChangingRole;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Player.ChangingRole -= OnChangingRole;
+            base.OnDisabled();
+        }
+
+        public void OnChangingRole(ChangingRoleEventArgs ev)
+        {
+            if (!ev.IsAllowed || ev.Reason == SpawnReason.ForceClass || !Config.blockList.Contains(ev.NewRole) ) return;
+            ev.NewRole = Config.swapList.RandomItem<RoleTypeId>();
+        }
+    }
+}
